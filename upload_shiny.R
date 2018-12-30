70cc7b45888021e6fcfbb33fd8b4acd2825af52b
georgios <georgios@caratheodory.Home>
2018-12-30 16:03:29 +0000

    1.7

--- upload_shiny.R
+++ upload_shiny.R
@@ -1,6 +1,7 @@
 rsconnect::deployApp('./demo_CLT/demo_CLT.Rmd',account ='georgios-stats')
 rsconnect::deployApp('./demo_distributions/demo_distributions.Rmd',account ='georgios-stats')
 rsconnect::deployApp('./demo_WLLN/demo_WLLN.Rmd',account ='georgios-stats')
+rsconnect::deployApp('./demo_Biostatistics_Bayes/demo_Biostatistics_Bayes.Rmd',account ='georgios-stats')
 
 rsconnect::deployApp('./demo_ConjugatePriors/demo_ConjugatePriors.Rmd',account ='georgios-stats-1')
 rsconnect::deployApp('./demo_ConjugateJeffreysLaplacePriors/demo_ConjugateJeffreysLaplacePriors.Rmd',account ='georgios-stats-1')
@@ -9,5 +10,6 @@ rsconnect::deployApp('./demo_PointEstimation/demo_PointEstimation.Rmd',account =
 rsconnect::deployApp('./demo_CredibleSets/demo_CredibleSets.Rmd',account ='georgios-stats-1')
 
 
+rsconnect::deployApp('./demo_MultivariateNormalDistribution/demo_MultivariateNormalDistribution.Rmd',account ='georgios-stats-3')
 
 
